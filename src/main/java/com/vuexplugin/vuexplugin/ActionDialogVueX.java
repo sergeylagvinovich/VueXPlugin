413bbb834e1b737eaa39ebeb44665ffe572ea46c
sergey <sergeylagvinovich@gmail.com>
2022-08-15 08:48:45 +0300

    menu

--- src/main/java/com/vuexplugin/vuexplugin/ActionDialogVueX.java
+++ src/main/java/com/vuexplugin/vuexplugin/ActionDialogVueX.java
@@ -1,47 +1,152 @@
 package com.vuexplugin.vuexplugin;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.ui.components.JBLabel;
-import com.intellij.uiDesigner.core.AbstractLayout;
-import com.intellij.util.ui.GridBag;
-import com.intellij.util.ui.JBUI;
-import com.intellij.util.ui.UIUtil;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.popup.PopupStep;
+import com.intellij.openapi.ui.popup.util.BaseListPopupStep;
+import com.intellij.openapi.util.NlsContexts;
 import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ActionDialogVueX extends DialogWrapper {
+public class ActionDialogVueX extends BaseListPopupStep<String> {
+    Editor editor;
+    Document document;
 
-
-    JPanel panel = new JPanel(new GridBagLayout());
-    JTextField txtMode = new JTextField();
-
-    protected ActionDialogVueX ( boolean canBeParent ) {
-        super (canBeParent);
+    String selectedValueInState;
+    static final ArrayList<String> itemPopUp = new ArrayList<>(){{
+        add("Create");
+        add("Getters and Setters");
+        add("Getters");
+        add("Setters");
+    }};
+    public ActionDialogVueX(Editor editor) {
+        super("Choose", itemPopUp);
+        this.editor = editor;
     }
 
     @Override
-    protected @Nullable JComponent createCenterPanel ( ) {
-        GridBag gb = new GridBag ()
-                .setDefaultInsets (JBUI.insets (0, 0, AbstractLayout.DEFAULT_VGAP, AbstractLayout.DEFAULT_HGAP))
-                .setDefaultWeightX (1.0)
-                .setDefaultFill (GridBagConstraints.HORIZONTAL);
-        panel.setPreferredSize (new Dimension (400,200));
-        panel.add (label ("mode"),gb.next ().weightx(0.2));
-        panel.add (txtMode,gb.next ().weightx(0.8));
-
-        return panel;
+    public @Nullable PopupStep<?> onChosen(String selectedValue, boolean finalChoice) {
+        System.out.println(selectedValue);
+        document = editor.getDocument();
+        selectedValueInState = editor.getSelectionModel ().getSelectedText ();
+        switch (selectedValue){
+            case "Create": {
+                create();
+                break;
+            }
+            case "Getters and Setters": {
+                makeGetters();
+                makeSetters();
+                break;
+            }
+            case "Getters": {
+                makeGetters();
+                break;
+            }
+            case "Setters": {
+                makeSetters();
+                break;
+            }
+            default:{
+                break;
+            }
+        }
+        if(!selectedValue.equals("Create")) {
+            makeState();
+        }
+        return super.onChosen(selectedValue, finalChoice);
     }
 
-    private JComponent label(String text){
 
-        JBLabel label = new JBLabel (text);
-        label.setComponentStyle (UIUtil.ComponentStyle.SMALL);
-        label.setFontColor (UIUtil.FontColor.BRIGHTER);
-        label.setBorder (JBUI.Borders.empty (0,5,2,0));
+    private void makeState(){
+        StringBuilder state = new StringBuilder ();
+        String documentText = document.getText (), str;
+        String[] params = selectedValueInState.split (",");
+        String fileName = getFileName();
+        for (String param :params) {
+            str = firstLetterToUpperCase (param);
+            state.append (param+":null,\n\t\t");
+        }
+        documentText = documentText.replaceAll(selectedValueInState,state.toString());
+        document.setText(documentText);
+    }
 
-        return label;
+    private void create(){
+        String createForm =
+                "export default {\n" +
+                        "\tnamespaced: true,\n"+
+                        "\tstate:{\n" +
+                        "\t\t//VueXState\n" +
+                        "\t},\n"+
+                        "\tgetters:{\n" +
+                        "\t\t//VueXGetters\n" +
+                        "\t},\n"+
+                        "\tmutations:{\n" +
+                        "\t\t//VueXMutations\n" +
+                        "\t},\n"+
+                        "\tactions:{\n" +
+                        "\t\t//VueXActions\n" +
+                        "\t},\n"+
+                "}";
+        try {
+            document.setText(createForm);
+        }
+        catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+    }
+
+    private void makeGetters(){
+        StringBuilder
+                getters = new StringBuilder (),
+                mapGetters = new StringBuilder()
+                ;
+        String documentText = document.getText (), str;
+        String[] params = selectedValueInState.split (",");
+        mapGetters.append ("//...mapGetters({\n\t");
+        String fileName = getFileName();
+        for (String param :params) {
+            str = firstLetterToUpperCase (param);
+            getters.append ("\t\tget"+str+"(state){\n\t\t\treturn state."+param+";\n\t\t},\n");
+            mapGetters.append ("//'get"+str+"':'"+fileName+"/get"+str+"',\n\t");
+        }
+        mapGetters.append ("//}),");
+        documentText = documentText.replaceAll("//VueXGetters","//VueXGetters\n"+getters.toString());
+        document.setText(documentText+"\n"+mapGetters.toString());
+    }
+
+    private void makeSetters(){
+        StringBuilder
+                mutations = new StringBuilder (),
+                mapMutations = new StringBuilder()
+                        ;
+        String documentText = document.getText (), str;
+        String[] params = selectedValueInState.split (",");
+        mapMutations.append ("//...mapMutations({\n\t");
+        String fileName = getFileName();
+        for (String param :params) {
+            str = firstLetterToUpperCase (param);
+            mutations.append ("\t\tset"+str+"(state,value){\n\t\t\tstate."+param+" = value;\n\t\t},\n");
+            mapMutations.append ("//'set"+str+"':'"+fileName+"/set"+str+"',\n\t");
+        }
+        mapMutations.append ("//}),");
+        documentText = documentText.replaceAll("//VueXMutations","//VueXMutations\n"+mutations.toString());
+        document.setText(documentText+"\n"+mapMutations.toString());
+    }
+
+    private String getFileName(){
+        String temp = document.toString ();
+        temp = temp.substring (temp.lastIndexOf ("/")+1).replaceAll (".js]","");
+        return temp;
+    }
+
+    private String firstLetterToUpperCase(String word){
+        if(word==null || word.length ()==0)
+            return word;
+        return word.substring (0,1).toUpperCase ()+word.substring (1);
     }
 }
--- src/main/java/com/vuexplugin/vuexplugin/VueXAction.java
+++ src/main/java/com/vuexplugin/vuexplugin/VueXAction.java
@@ -9,73 +9,20 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileChooser.ex.FileChooserKeys;
 import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.jetbrains.rd.util.string.StingUtilKt;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 
 public class VueXAction extends AnAction {
 
     @Override
     public void actionPerformed ( @NotNull AnActionEvent e ) {
-
-            if(new ActionDialogVueX (true).showAndGet ()){
-
-            }
-
-
-//        String str,selectedValueInState,documentText, fileName;
-//
-//        StringBuilder
-//                getters = new StringBuilder (),
-//                mutations = new StringBuilder (),
-//                state = new StringBuilder (),
-//                mapGetters = new StringBuilder (),
-//                mapMutations = new StringBuilder ();
-//
-//        Editor editor = e.getData (PlatformDataKeys.EDITOR);
-//        assert editor != null;
-//
-//        selectedValueInState = editor.getSelectionModel ().getSelectedText ();
-//
-//        Document document = editor.getDocument ();
-//        documentText = document.getText ();
-//
-//        String[] params = selectedValueInState.split (",");
-//
-//        mapGetters.append ("...mapGetters({\n\t");
-//        mapMutations.append ("...mapMutations({\n\t");
-//        fileName = getFileName(document);
-//        for (String param :params) {
-//            str = firstLetterToUpperCase (param);
-//            getters.append ("get"+str+"(state){\n\treturn state."+param+";\n},\n");
-//            mutations.append ("set"+str+"(state,value){\n\tstate."+param+" = value;\n},\n");
-//            state.append (param+":null,\n\t\t");
-//            mapGetters.append ("'get"+str+"':'"+fileName+"/get"+str+"',\n\t");
-//            mapMutations.append ("'set"+str+"':'"+fileName+"/set"+str+"',\n\t");
-//        }
-//        mapGetters.append ("}),");
-//        mapMutations.append ("}),");
-//        documentText = documentText.replace (selectedValueInState,state);
-//        document.setText (getters+"\n"+mutations+"\n"+mapGetters+"\n"+mapMutations+"\n"+documentText);
-    }
-
-
-    private String firstLetterToUpperCase(String word){
-        if(word==null || word.length ()==0)
-            return word;
-        return word.substring (0,1).toUpperCase ()+word.substring (1);
-    }
-
-    private String getFileName(Document document){
-        String temp = document.toString ();
-        temp = temp.substring (temp.lastIndexOf ("/")+1).replaceAll (".js]","");
-        return temp;
-    }
-
-    @Override
-    public boolean isDumbAware ( ) {
-        return false;
-//        return super.isDumbAware ();
+        Editor editor = e.getData (PlatformDataKeys.EDITOR);
+        ActionDialogVueX  dialog = new ActionDialogVueX(editor);
+        JBPopupFactory.getInstance().createListPopup(dialog,5).showInFocusCenter();
     }
 }
